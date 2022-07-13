edb6c7943e09e49f0df053d354f2851efb96e9f4
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-07-13 04:00:47 -0300

    Atualização Do Projeto Final

--- ControleDeContatos/ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/ControleDeContatos/Models/UsuarioModel.cs
@@ -1,15 +1,21 @@
 ﻿using ControleDeContatos.Enuns;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleDeContatos.Models
 {
     public class UsuarioModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio!")]
         public string Login { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio!")]
+        [EmailAddress(ErrorMessage = "E-mail inválido ")]
         public string Email { get; set; }
 
         public PerfilEnum Perfil { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio!")]
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
